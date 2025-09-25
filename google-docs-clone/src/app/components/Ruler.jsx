042b24484d8fbfc6deaa4bf3cf682c146034c1b8
sameep25 <sameepvishwakarma@gmail.com>
2025-09-25 15:07:17 +0530

    Fixes the css of ruler

--- google-docs-clone/src/app/components/Ruler.jsx
+++ google-docs-clone/src/app/components/Ruler.jsx
@@ -65,12 +65,9 @@ const Ruler = () => {
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
-      className="h-8 border-b border-gray-300 flex items-end relative select-none print:hidden"
+      className="w-[816px] mx-auto h-8 border-b border-gray-300 flex items-end relative select-none print:hidden"
     >
-      <div
-        id="ruler-container"
-        className="max-w-[816px] mx-auto w-full h-full relative"
-      >
+      <div id="ruler-container" className="w-full h-full relative">
         {/* Left Marker */}
         <PaddingMarker
           position={leftMargin}
